a5b9bae2073d8a208977948a5b95ccbbc2b8ea17
MegaGeekster <leahc0831@gmail.com>
2026-01-23 01:42:30 +0200

    Add name and icon to settings window

--- Grid100Game/settingsdialog.cpp
+++ Grid100Game/settingsdialog.cpp
@@ -6,6 +6,8 @@ SettingsDialog::SettingsDialog(QWidget *parent)
     , ui(new Ui::SettingsDialog)
 {
     ui->setupUi(this);
+    this->setWindowIcon(QIcon(":/icons/Game.ico"));
+    this->setWindowTitle("Settings");
 }
 
 SettingsDialog::~SettingsDialog()
